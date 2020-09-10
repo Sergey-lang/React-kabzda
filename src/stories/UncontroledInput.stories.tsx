b48aca3cb78b5eeec83ecd3476cb73a7983abda1
Sergey-Lang <63201050+Sergey-lang@users.noreply.github.com>
2020-09-10 22:59:06 +0300

    controled component

--- src/stories/UncontroledInput.stories.tsx
+++ src/stories/UncontroledInput.stories.tsx
@@ -1,5 +1,6 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {Input} from './Input';
+import {action} from '@storybook/addon-actions';
 
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 
@@ -9,3 +10,30 @@ export default {
 }
 
 export const ControledInputStories = () => <Input/>
+
+export const ControledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChange}/>
+}
+export const ControledCheckBox = () => {
+    const [checkValue, setCheckValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheckValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' onChange={onChange} checked={checkValue}/>
+}
+export const ControledSelect = () => {
+    const [selectValue, setDelectValue] = useState<string | undefined>('2')
+    const onSelChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setDelectValue(e.currentTarget.value)
+    }
+    return <select value={selectValue} onChange={onSelChange}>
+        <option value="1">Minsk</option>
+        <option value="2">USA</option>
+        <option value="3">Russia</option>
+        <option value="4">Poland</option>
+    </select>
+}
