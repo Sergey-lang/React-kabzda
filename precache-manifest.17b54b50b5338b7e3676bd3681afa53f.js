7e45d944ba34df30b868eedb9979a5ab16ccb836
Sergey-lang <sergei.kuharyonok@yandex.by>
2020-09-18 18:13:35 +0300

    Updates

---
+++ precache-manifest.17b54b50b5338b7e3676bd3681afa53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93350df404d060faeb4715ff8666d996",
+    "url": "/React-kabzda/index.html"
+  },
+  {
+    "revision": "cf8a09d06ce208c34057",
+    "url": "/React-kabzda/static/css/main.6c69b40a.chunk.css"
+  },
+  {
+    "revision": "16005338c6a73496e2e5",
+    "url": "/React-kabzda/static/js/2.36012bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-kabzda/static/js/2.36012bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8a09d06ce208c34057",
+    "url": "/React-kabzda/static/js/main.c92a9311.chunk.js"
+  },
+  {
+    "revision": "f9c32cf8784a674779b1",
+    "url": "/React-kabzda/static/js/runtime-main.a024a2bb.js"
+  }
+]);
